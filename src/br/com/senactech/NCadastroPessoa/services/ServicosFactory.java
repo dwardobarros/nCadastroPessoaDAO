89bb014e7b46442f522e9161eb53b765dbfe39f8
dwardobarros <dwardobarros@gmail.com>
2022-06-20 21:54:10 -0300

    Factory Services

---
+++ src/br/com/senactech/NCadastroPessoa/services/ServicosFactory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senactech.NCadastroPessoa.services;
+
+/**
+ *
+ * @author 666
+ */
+public class ServicosFactory {
+    
+}
