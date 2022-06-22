68998ac2498323d432a7ab0e3aaf05f46f419d5a
dwardobarros <dwardobarros@gmail.com>
2022-06-21 21:30:53 -0300

    CRUD concluído

--- src/br/com/senactech/NCadastroPessoa/dao/PessoaDAO.java
+++ src/br/com/senactech/NCadastroPessoa/dao/PessoaDAO.java
@@ -18,50 +18,49 @@ import java.util.ArrayList;
  * @author 666
  */
 public class PessoaDAO {
-    
-    public void cadastrarPessoa(Pessoa pVO) throws SQLException{
+
+    public void cadastrarPessoa(Pessoa pVO) throws SQLException {
         //Buscar conexao do BD
         Connection con = Conexao.getConexao();
         //Cria espaço de trabalho SQL, é a área no Java onde vamos executar  os scripts SQL
         Statement stat = con.createStatement();
-        
+
         try {
-       String sql;
-                sql = "insert into pessoas values"
-               + "null, '"
-               + pVO.getNomePessoa() + "', " 
-               + pVO.getCpf() + "', " 
-               + pVO.getEndereco() + "', " 
-               + pVO.getTelefone() + "', "
-               +pVO.getIdade() + ", "
-               +pVO.isStatus() + ";";
-       
-       stat.execute(sql);
-       
+            String sql;
+            sql = "insert into pessoa values (null, '"
+                    + pVO.getNomePessoa() + "', '"
+                    + pVO.getCpf() + "','"
+                    + pVO.getEndereco() + "', '"
+                    + pVO.getTelefone() + "', '"
+                    + pVO.getIdade() + "', "
+                    + pVO.isStatus() + ");";
+
+            stat.execute(sql);
+
         } catch (SQLException e) {
 
-        throw new SQLException("Erro ao inserir Pessoa! \n" + e.getMessage()) ;
-        
-         } finally {
+            throw new SQLException("Erro ao inserir Pessoa! \n" + e.getMessage());
+
+        } finally {
             con.close();
             stat.close();
+        }
+
     }
-        
-    }
-    
+
     public ArrayList<Pessoa> listarPessoas() throws SQLException {
-        
-         //Buscar conexao do BD
+
+        //Buscar conexao do BD
         Connection con = Conexao.getConexao();
         //Cria espaço de trabalho SQL, é a área no Java onde vamos executar  os scripts SQL
         Statement stat = con.createStatement();
-        
+
         try {
             String sql;
             sql = "select * from pessoa";
             ResultSet rs = stat.executeQuery(sql);
             ArrayList<Pessoa> pessoas = new ArrayList<>();
-            while(rs.next()){
+            while (rs.next()) {
                 Pessoa p = new Pessoa();
                 //Lado do java |x| lado do BD
                 p.setIdPessoa(rs.getInt("idPessoa"));
@@ -72,30 +71,29 @@ public class PessoaDAO {
                 p.setIdade(rs.getInt("idade"));
                 p.setStatus(rs.getBoolean("status"));
                 //popula o arraylist
-                pessoas.add(p);   
+                pessoas.add(p);
             }
             //retorna arrayList
             return pessoas;
-            
+
         } catch (SQLException e) {
             throw new SQLException("Erro ao listar Pessoas! \n" + e.getMessage());
-            
+
         } finally {
             con.close();
             stat.close();
-       
 
         }
     }
-    
-    public boolean verCPF(String cpf) throws SQLException{
-         //Buscar conexao do BD
+
+    public boolean verCPF(String cpf) throws SQLException {
+        //Buscar conexao do BD
         Connection con = Conexao.getConexao();
         //Cria espaço de trabalho SQL, é a área no Java onde vamos executar  os scripts SQL
         Statement stat = con.createStatement();
-        
+
         boolean verCPF = false;
-        
+
         try {
             String sql;
             sql = "select cpf from pessoa where cpf = " + cpf;
@@ -103,30 +101,30 @@ public class PessoaDAO {
             while (rs.next()) {
                 verCPF = rs.wasNull();
             }
-    } catch (SQLException e) {
+        } catch (SQLException e) {
             throw new SQLException("CPF Inexistente! \n" + e.getMessage());
-           } finally {
+        } finally {
             con.close();
             stat.close();
-       
+
         }
         return verCPF;
     }
-    
-    public Pessoa getByDoc(String cpf) throws SQLException{
+
+    public Pessoa getByDoc(String cpf) throws SQLException {
         //Buscar conexao do BD
         Connection con = Conexao.getConexao();
         //Cria espaço de trabalho SQL, é a área no Java onde vamos executar  os scripts SQL
         Statement stat = con.createStatement();
-        
+
         Pessoa p = new Pessoa();
-        
+
         try {
             String sql;
             sql = "select * from pessoa where cpf =" + cpf;
             ResultSet rs = stat.executeQuery(sql);
-                while(rs.next()) {
-                    //lado do java |x| lado do banco
+            while (rs.next()) {
+                //lado do java |x| lado do banco
                 p.setIdPessoa(rs.getInt("idPessoa"));
                 p.setNomePessoa(rs.getString("nomePessoa"));
                 p.setCpf(rs.getString("cpf"));
@@ -134,22 +132,22 @@ public class PessoaDAO {
                 p.setTelefone(rs.getString("telefone"));
                 p.setIdade(rs.getInt("idade"));
                 p.setStatus(rs.getBoolean("status"));
-                }
+            }
         } catch (SQLException e) {
-                throw new SQLException("Pessoa inexistente! \n" + e.getMessage());
+            throw new SQLException("Pessoa inexistente! \n" + e.getMessage());
         } finally {
             con.close();
             stat.close();
         }
         return p;
     }
-    
+
     public void deletarPessoa(int id) throws SQLException {
         //Buscar conexao do BD
         Connection con = Conexao.getConexao();
         //Cria espaço de trabalho SQL, é a área no Java onde vamos executar  os scripts SQL
         Statement stat = con.createStatement();
-        
+
         try {
             String sql;
             sql = "delete from pessoa where idPessoa = " + id;
@@ -161,21 +159,21 @@ public class PessoaDAO {
             stat.close();
         }
     }
-    
-    public void atualizarPessoa(Pessoa pVO) throws SQLException{
+
+    public void atualizarPessoa(Pessoa pVO) throws SQLException {
         //Buscar conexao do BD
         Connection con = Conexao.getConexao();
         //Cria espaço de trabalho SQL, é a área no Java onde vamos executar  os scripts SQL
         Statement stat = con.createStatement();
-        
+
         try {
             String sql;
             sql = "update pessoa set "
-                    + "nomePessoa = '" + pVO.getNomePessoa() +  "', "
-                    + "endereco = '" + pVO.getEndereco() +  "', "
-                    + "idade = " + pVO.getIdade() +  ", "
-                    + "telefone = '" + pVO.getTelefone() +  "', "
-                    + "status = " + pVO.isStatus() +  " "
+                    + "nomePessoa = '" + pVO.getNomePessoa() + "', "
+                    + "endereco = '" + pVO.getEndereco() + "', "
+                    + "idade = " + pVO.getIdade() + ", "
+                    + "telefone = '" + pVO.getTelefone() + "', "
+                    + "status = " + pVO.isStatus() + " "
                     + "where idPessoa = " + pVO.getIdPessoa();
             stat.executeUpdate(sql);
         } catch (SQLException e) {
@@ -183,7 +181,7 @@ public class PessoaDAO {
         } finally {
             con.close();
             stat.close();
-     }
+        }
     }
-    
+
 }
